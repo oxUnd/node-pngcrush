10f380651fb2eb6434f69a547552378ba6fe64bf
xiangshouding <fansekey@gmail.com>
2020-06-03 15:15:55 +0800

    [UPDATE] 1.1.0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var semver = require('semver');
-var _handle = require('./scripts/util/binding.js')();
+var _handle = require('./scripts/binding.js')();
 
 function PngCompress(buffer) {
     this._option = '';
